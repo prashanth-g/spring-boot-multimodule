62d1b055f510954941013d4a374d3c95dc344f10
prashanth-g <prashanthcsgce@gmail.com>
2019-09-04 12:14:29 +0530

    Added base package scanning in application

--- application/src/main/java/com/prashanth/os/spring/multimodule/application/MultimoduleApplication.java
+++ application/src/main/java/com/prashanth/os/spring/multimodule/application/MultimoduleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "hello")
+@SpringBootApplication(scanBasePackages = "com.prashanth.os.spring.multimodule")
 @RestController
 public class MultimoduleApplication {
 
