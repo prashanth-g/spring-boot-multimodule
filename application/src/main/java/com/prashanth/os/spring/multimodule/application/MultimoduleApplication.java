6392ae636114b706303e421d9cd342adf3848ae9
prashanth-g <prashanthcsgce@gmail.com>
2019-09-04 12:12:54 +0530

    Added application

---
+++ application/src/main/java/com/prashanth/os/spring/multimodule/application/MultimoduleApplication.java
@@ -0,0 +1,27 @@
+package com.prashanth.os.spring.multimodule.application;
+
+import com.prashanth.os.spring.multimodule.library.LibraryService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "hello")
+@RestController
+public class MultimoduleApplication {
+
+  private final LibraryService libraryService;
+
+  public MultimoduleApplication(LibraryService libraryService) {
+    this.libraryService = libraryService;
+  }
+
+  @GetMapping("/")
+  public String home() {
+    return libraryService.message();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(MultimoduleApplication.class, args);
+  }
+}
